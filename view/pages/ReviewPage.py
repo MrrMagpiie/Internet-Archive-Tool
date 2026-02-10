da17bc3d402e98e44fa857d30ae853f163415f46
MrrMagpiie <Github@droptoinbox.com>
2026-02-10 09:36:49 -0500

    Review page now shows documents ready to be uploaded

--- view/pages/ReviewPage.py
+++ view/pages/ReviewPage.py
@@ -1,37 +1,105 @@
 import sys
 from PyQt6.QtWidgets import (
-    QWidget, QVBoxLayout,QGridLayout,
-    QPushButton, QLabel
+    QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, 
+    QListWidget, QStackedWidget, QLabel, QPushButton, QSplitter, 
+    QListWidgetItem, QFrame, QFormLayout, QLineEdit, QDateEdit,
+    QProgressBar, QComboBox, QGraphicsView, QGraphicsScene,QGridLayout,
+    QScrollArea,QSizePolicy, QLayout
 )
+from PyQt6.QtCore import Qt, QSize, pyqtSignal, QRect, QPoint, pyqtSlot,QObject
+from PyQt6.QtGui import QIcon, QFont, QColor, QCursor
 
-from view.components.ActionCard import ActionCard
-from view.components.ActionDashboard import ActionDashboard
 from view.components.Page import Page
+from view.components.CenteredFlowLayout import CenteredFlowLayout
+from view.components.DashboardDocumentCard import DocumentCard
+from model.data.document import Document
+from model.logic.helpers import clear_layout
+
+
 
 class ReviewPage(Page):
-    '''
-    frontpage of application for choose actions
-    '''
+    project_selected = pyqtSignal(object,int)
+    db_request = pyqtSignal(dict,QObject)
 
-    def __init__(self,navigation_stack,parent=None):
-        super().__init__(parent)
-        self.stack = navigation_stack
-        self._create_layout()
-
-    def _create_layout(self):
-        self.main_layout = QVBoxLayout(self)
-        self.main_layout.setContentsMargins(30, 30, 30, 30) 
-        title = "Master Review Page"
-        description = "will Show Documents ready to upload once reviewed."
-       
-        # Title Section
-        header = QLabel(f"<span id='stepTitle' style='font-size: 24pt; font-weight: 600;'>{title}</span>")
-        header.setObjectName("dashHeader")
-        subtitle = QLabel(f"<span id='stepDesc' style='font-size: 11pt;'>{description}</span>")
-        subtitle.setObjectName("dashSubtitle")
+    def __init__(self,parent):
+        super().__init__(parent=parent)
+        self.docs = []
+        # Main Layout of the Page
+        main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(20, 20, 20, 20)
+        main_layout.setSpacing(40)
+        # Header Section
+        title = QLabel("Dashboard")
+        title.setFont(QFont("Segoe UI", 20, QFont.Weight.Bold))
         
-        self.main_layout.addWidget(header)
-        self.main_layout.addWidget(subtitle)
-        self.main_layout.addSpacing(20)
+        main_layout.addWidget(title)
+        
+        recent_docects_header = QLabel("Recent Documents")
+        recent_docects_header.setFont(QFont("Segoe UI", 16, QFont.Weight.Bold))
+        main_layout.addWidget(recent_docects_header)
 
+        # --- The Scroll Area for the Grid ---
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        scroll_area.setFrameShape(QFrame.Shape.NoFrame)
+        scroll_area.setStyleSheet("background-color: transparent;") # Let window bg show
+        # --- The Scroll Area for the Grid ---
+
+       
+        # The container widget that holds the grid
+        grid_container = QWidget()
+        
+        self.layout_flow = CenteredFlowLayout()  
+        self.layout_flow.setSpacing(20)
+        self.layout_flow.setContentsMargins(20, 20, 20, 20)   # <-- ADD THIS (pass margins if needed)
+        
+        grid_container.setLayout(self.layout_flow)
+        scroll_area.setWidget(grid_container)
+        
+        main_layout.addWidget(scroll_area)
+        self.setLayout(main_layout)
+        
+
+    @pyqtSlot(Document,int)
+    def card_clicked(self,doc,stage=1):
+        if doc == None:
+            print('Create New Document Clicked')
+        else:
+            print(f'{doc.doc_id} Card Clicked')
+        self.project_selected.emit(doc,stage)
+
+    @pyqtSlot(list)
+    def show_documents(self):
+        clear_layout(self.layout_flow)
+        for document in self.docs:
+            doc, stage = document
+            card = DocumentCard(doc,stage)
+            card.clicked.connect(self.card_clicked)
+            self.layout_flow.addWidget(card)
+
+    def load_documents(self,documents):
+        self.docs = []
+        for doc in documents.values():
+            if doc.status['metadata'] == False:
+                self.docs.append((doc,1))
+            elif doc.status['deskewed'] == True:
+                self.docs.append((doc,2))
+            else:
+                self.docs.append((doc,0))
+
+    @pyqtSlot(dict)
+    def request_documents(self):
+        filter_data = {'needs_approval':True}
+        self.db_request.emit(filter_data,self)
+
+    @pyqtSlot(str,object)
+    def doc_return(self,command,docs):
+        self.load_documents(docs)
+        self.show_documents()
+
+    @pyqtSlot(str)
+    def doc_error(self,error_msg):
+        print(f'db_error: {error_msg}')
 
