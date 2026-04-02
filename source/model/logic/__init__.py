adebb3c8bb64fff8a320d7d6d35747d82a42cea9
MrrMagpiie <Github@droptoinbox.com>
2026-04-02 16:48:59 -0400

    upload import fix

--- source/model/logic/__init__.py
+++ source/model/logic/__init__.py
@@ -2,6 +2,5 @@ from .deskew import *
 from .discover import *
 from .helpers import *
 from .metadata import *
-from .upload import *
+#from .upload import *
 from .loadImage import *
-from .upload import *
